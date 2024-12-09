49de3b7dd89eb07a8f17e9189276a1b0363f65f3
Jens Kaiser <jens.kaiser@codecentric.de>
2024-12-09 21:45:53 +0100

    update freshmarker versions

--- src/test/java/de/schegge/freshmarker/demo/EmailServiceTest.java
+++ src/test/java/de/schegge/freshmarker/demo/EmailServiceTest.java
@@ -7,9 +7,7 @@ import de.schegge.test.mail.SmtpTest;
 import jakarta.mail.MessagingException;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 import java.nio.file.Path;
